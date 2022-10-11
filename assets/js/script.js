e171c8dc0db46d7756746c0f78629e63298658fc
unknown <ebussman@gmail.com>
2022-10-11 11:41:27 -0700

    updated background color and task color

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,34 +3,34 @@ let submitButton = $('#submitBtn');
 let firstName = $('#first_name');
 let userMotivator = $('#motivator');
 let userInfo = {};
-let exp = 0;
-let level = 1;
-var easyEXP = document.querySelector("#easy");
-var mediumEXP = document.querySelector("#medium");
-var hardEXP = document.querySelector("#hard");
-let lightMode = true;
-let darkMode = false;
+
 // Hobby Page Variables
 let userNameHere = $('#userNameHere');
 let taskButton = $('#taskBtn');
 let listedTasks = $('#taskList');
-let modeToggle = $(`input:checkbox`);
-let youtubeAPIKey = `AIzaSyAeZ3OPG8Md9rwhI3CzE3KoUWYC45JHKWw`;
+let youtubeAPIKey = `AIzaSyAeZ3OPG8Md9rwhI3CzE3KoUWYC45JHKWw`
 
 
 // Stores the user's info locally and then changes the page to the main hobby tracker page
 submitButton.on('click', function() {
     console.log("clicked");
-
+    
+    let localStoredTaskList = [];
+    console.log(localStoredTaskList);
 
     let userInfo ={
         name: firstName.val(),
         motivator: userMotivator.val()
     };
 
-    localStorage.setItem("User", JSON.stringify(userInfo));
+    localStoredTaskList.push(userInfo);
+    console.log(localStoredTaskList);
 
-    window.location.assign("./../index.html");
+
+    localStorage.setItem("User", JSON.stringify(userInfo));
+    localStorage.setItem("LocalStoredTasks", JSON.stringify(localStoredTaskList));
+
+    window.location.assign("file:///C:/Users/Erik/code/PineappleMotivator/index.html");
 
 });
 
@@ -48,21 +48,19 @@ loadUserData();
 // Turns numeric values from dropdown input into words
 function dropdownTranslate2() {
     let storedTasks = JSON.parse(localStorage.getItem("Task"));
+    let div = document.querySelector("#taskDiv");
+    let checkbox = document.querySelector('#checkboxHidden');
+    let clonedCheckbox = checkbox.cloneNode(true);
 
-    let label = document.createElement("label");
-    let createInput = document.createElement("input");
-    createInput.setAttribute("type", "checkbox");
-    
-
-
+    clonedCheckbox.id = 'subtaskCheckbox';
+    clonedCheckbox.classList.remove("hide");
+  
     if (storedTasks.subtasks == 1) {
-        return div.append(label), label.append(createInput);
+        div.append(clonedCheckbox);
     } else if (storedTasks.subtasks == 2) {
-        return div.append(label), label.append(createInput), div.append(label), label.append(createInput);
+        div.append(clonedCheckbox), div.append(clonedCheckbox);
     } else (storedTasks.subtasks == 3) 
-        return div.append(label), label.append(createInput), div.append(label), label.append(createInput), div.append(label), label.append(createInput);
-    
-
+        div.append(clonedCheckbox), div.append(clonedCheckbox), div.append(clonedCheckbox);
 };
 
 function dropdownTranslate1() {
@@ -90,23 +88,24 @@ function createCard() {
     let h3 = document.createElement("h3");
     let h4 = document.createElement("h4");
     let p = document.createElement("p");
-    let textarea = document.createElement("textarea");
+    let checkbox = document.querySelector('#checkboxHidden');
+    let clonedCheckbox = checkbox.cloneNode(true);
+
     
     
     div.setAttribute("class", "task container z-depth-3 p-2");
+    div.id = "taskDiv";
     h3.innerText = storedTasks.taskName;
     h4.innerText = dropdownTranslate1();
-    textarea.innerText = "Notes go here!";
-    textarea.setAttribute("class", "white");
+    clonedCheckbox.id = 'subtaskCheckbox';
+    clonedCheckbox.classList.remove("hide");
            
     
     listedTasks.append(div);
     div.append(h3);
     div.append(h4);
     div.append(p);
-    div.append(textarea);
-
-
+    div.append(clonedCheckbox);
 };
 
 // Modal function
@@ -139,9 +138,8 @@ function createTask() {
     createCard();
 };
 
-
 // proof of concept youtube API fetch
-fetch (`https://www.googleapis.com/youtube/v3/search?part=snippet&q=dogs&key=AIzaSyAeZ3OPG8Md9rwhI3CzE3KoUWYC45JHKWw`)
+fetch (`https://www.googleapis.com/youtube/v3/search?part=snippet&q=cats&key=AIzaSyAeZ3OPG8Md9rwhI3CzE3KoUWYC45JHKWw`)
 .then (function(response) {
     return response.json()
 })
@@ -159,100 +157,12 @@ fetch (`https://motivational-quote-api.herokuapp.com/quotes/random`)
     console.log(data)
 })
 
+// // proof of concept Pirate API fetch
+// fetch("https://pirate.monkeyness.com/api/insult")
+// .then(function(response)
+// {console.log(response);
+//     //return response.json();
+//     }).then(function(data){
+//     console.log(data)
+//     })
 
-// proof of concept pirate translator api
-function displayPirate(pwords) {
-    console.log(pwords)
-    var pirateSec = document.querySelector("#motivating")
-    pirateSec.textContent = pwords.contents.translated
-    console.log(pwords.contents.translated)
-}
-
-var pirateURL = "https://api.funtranslations.com/translate/pirate.json?text=Hello%20sir%21%20my%20mother%20goes%20with%20me%20to%20the%20ocean"
-fetch(pirateURL)
-    .then(function (response) {
-        console.log(response);
-        if (response.ok) {
-            response.json().then(function (data) {
-
-                displayPirate(data);
-            })
-        }
-    })
-
-    // ======================================================================== 
-
-function updateEasy()
-{
-    exp += 25;
-    
-    if(exp >= 100)
-    {
-        level += 1;
-        //add a level up
-        document.querySelector(".levelPrcnt").textContent = "LEVELED UP!";
-        document.querySelector(".skillLevel").textContent = `${"Level: " + level}`;
-        exp -= 100;
-    }
-  document.querySelector(".levelFill").style.width = `${exp}%`;
-  document.querySelector(".levelPrcnt").textContent = `${exp}%`;
-}
-
-function updateMedium()
-{
-    exp += 50;
-
-    if(exp >= 100)
-    {
-        level += 1;
-        //add a level up
-        document.querySelector(".skillLevel").textContent = `${"Level: " + level}`;
-        exp -= 100;
-    }
-
-  document.querySelector(".levelFill").style.width = `${exp}%`;
-  document.querySelector(".levelPrcnt").textContent = `${exp}%`;
-}
-
-function updateHard()
-{
-    exp += 75;
-
-    if(exp >= 100)
-    {
-        level += 1;
-        //add a level up
-        document.querySelector(".skillLevel").textContent = `${"Level: " + level}`;
-        exp -= 100;
-    }
-
-
-  document.querySelector(".levelFill").style.width = `${exp}%`;
-  document.querySelector(".levelPrcnt").textContent = `${exp}%`;
-}
-
-  easyEXP.addEventListener("click", updateEasy);
-  mediumEXP.addEventListener("click", updateMedium);
-  hardEXP.addEventListener("click", updateHard);
-
-//   dark mode/light mode functions
-function changeDark () {
-    console.log(`I will change to dark mode`)
-}
-function changeLight () {
-    console.log(`I will change to light mode`)
-}
-
-
-// dark mode toggle
-modeToggle.on(`change`, function () {
-    if (lightMode === true) {
-        lightMode = false;
-        darkMode = true;
-        changeDark();
-    } else {
-        lightMode = true;
-        darkMode = false;
-        changeLight();
-    }
-})
